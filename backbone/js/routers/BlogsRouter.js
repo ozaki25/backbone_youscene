a5a4b2fb40a3ceb661f05c9aaa74367769671fc7
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-03-23 16:59:11 +0900

    MarionetteのappRouterを使ったものに変更

--- backbone/js/routers/BlogsRouter.js
+++ backbone/js/routers/BlogsRouter.js
@@ -1,6 +1,7 @@
 var $ = require('jquery');
 var _ = require('underscore');
 var Backbone = require('backbone');
+var Marionette = require('backbone.marionette');
 var Blog = require('../models/Blog');
 var Blogs = require('../collections/Blogs');
 var IndexView = require('../views/blogs/IndexView');
@@ -8,49 +9,51 @@ var NewView = require('../views/blogs/NewView');
 var EditView = require('../views/blogs/EditView');
 var ShowView = require('../views/blogs/ShowView');
 
-module.exports =  Backbone.Router.extend({
-    routes : {
+module.exports =  Marionette.AppRouter.extend({
+    appRoutes : {
         ''               : 'index',
         'blogs'          : 'index',
         'blogs/new'      : 'newBlog',
         'blogs/:id/edit' : 'edit',
         'blogs/:id'      : 'show'
     },
-    index : function index() {
-        console.log('Rotuer', 'index', new Date());
-        var blogs = new Blogs();
-        var index = new IndexView({collection: blogs});
-        index.render();
-    },
-    newBlog : function newBlog() {
-        console.log('Router', 'newBlog', new Date());
-        var blogs = new Blogs();
-        var newBlog = new NewView({collection: blogs});
-        newBlog.render();
-    },
-    edit : function edit(id) {
-        console.log('Router', 'edit', new Date());
-        var blog = new Blog({id: id});
-        blog.fetch()
-            .done(function() {
-                var Edit = new EditView({model: blog});
-                Edit.render();
-            })
-            .fail(function() {
-                console.log('Router', 'blog fetch failed', new Date());
-            });
-    },
-    show : function show(id) {
-        console.log('Router', 'show', new Date());
-        var blog = new Blog({id: id});
-        blog.fetch()
-            .done(function() {
-                var Show = new ShowView({model: blog});
-                Show.render();
-            })
-            .fail(function() {
-                console.log('Router', 'blog fetch failed', new Date());
-            });
+    controller: {
+        index : function index() {
+            console.log('Rotuer', 'index', new Date());
+            var blogs = new Blogs();
+            var index = new IndexView({collection: blogs});
+            index.render();
+        },
+        newBlog : function newBlog() {
+            console.log('Router', 'newBlog', new Date());
+            var blogs = new Blogs();
+            var newBlog = new NewView({collection: blogs});
+            newBlog.render();
+        },
+        edit : function edit(id) {
+            console.log('Router', 'edit', new Date());
+            var blog = new Blog({id: id});
+            blog.fetch()
+                .done(function() {
+                    var Edit = new EditView({model: blog});
+                    Edit.render();
+                })
+                .fail(function() {
+                    console.log('Router', 'blog fetch failed', new Date());
+                });
+        },
+        show : function show(id) {
+            console.log('Router', 'show', new Date());
+            var blog = new Blog({id: id});
+            blog.fetch()
+                .done(function() {
+                    var Show = new ShowView({model: blog});
+                    Show.render();
+                })
+                .fail(function() {
+                    console.log('Router', 'blog fetch failed', new Date());
+                });
+        }
     }
-
 });
+
