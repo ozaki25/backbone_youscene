2e1fde659a0854b401ccfb4f826385a2c9955ea3
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-04-20 14:30:44 +0900

    コメントを削除できるようにした

--- app/scripts/views/comments/CommentView.js
+++ app/scripts/views/comments/CommentView.js
@@ -4,6 +4,20 @@ var Framework = require('../../vendor/Framework');
 module.exports = Framework.ItemView.extend({
     moduleName: 'comment/CommentView',
     tagName:  'div',
-    template: '#comment_view'
+    template: '#comment_view',
+    ui: {
+        destroy: '.close'
+    },
+    events: {
+        'click @ui.destroy': 'destroyComment'
+    },
+    destroyComment: function() {
+        var isDestroy = confirm('削除してよろしいですか。');
+        if(isDestroy) {
+            this.model.destroy()
+        } else {
+            return;
+        }
+    }
 });
 
